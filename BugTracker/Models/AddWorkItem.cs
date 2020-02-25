2fbf5572da5e7a296bb79afa488c90b23797f16e
va1ery <valery.gorov@gmail.com>
2020-02-25 10:14:55 +0300

    5-th commit

---
+++ BugTracker/Models/AddWorkItem.cs
@@ -0,0 +1,8 @@
+public class AddWorkItem 
+{ 
+    public string Title { get; set; } 
+    public string Description { get; set; } 
+    public int Severity { get; set; } 
+    public string WorkItemType { get; set; } 
+    public string AssignedTo { get; set; } 
+}
